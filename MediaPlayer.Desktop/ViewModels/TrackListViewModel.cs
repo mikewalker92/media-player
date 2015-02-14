3646d5c29386b540eafa115345ef80b994b74519
= <mikew.haske@gmail.com>
2015-02-14 02:40:17 +0000

    First pass of track list view.

--- MediaPlayer.Desktop/ViewModels/TrackListViewModel.cs
+++ MediaPlayer.Desktop/ViewModels/TrackListViewModel.cs
@@ -2,17 +2,57 @@
 {
     using Caliburn.Micro;
     using System.Windows.Controls;
+    using System.Data;
 
     public class TrackListViewModel : Screen
     {
-        protected override void OnViewLoaded(object view)
+        private DataTable _trackData;
+
+        #region Properties
+        public DataTable TrackData
         {
+            get
+            {
+                return _trackData;
+            }
+
+            set
+            {
+                _trackData = value;
+                NotifyOfPropertyChange(() => TrackData);
+            }
+        }
+#endregion
+
+        public TrackListViewModel()
+        {
+            CreateTable();
             PopulateTrackList();
         }
 
-        public void PopulateTrackList()
+        private void CreateTable()
         {
+            TrackData = new DataTable();
+            TrackData.Columns.Add(new DataColumn("Title", typeof(string)));
+            TrackData.Columns.Add(new DataColumn("Artist", typeof(string)));
+            TrackData.Columns.Add(new DataColumn("Album", typeof(string)));
+            TrackData.Columns.Add(new DataColumn("Length", typeof(string)));
+            TrackData.Columns.Add(new DataColumn("Plays", typeof(string)));
+        }
 
+        private void PopulateTrackList()
+        {
+            // TODO - this method os obviously not finished!
+            for (var counter = 0; counter < 100; counter++)
+            {
+                var row = TrackData.NewRow();
+                row["Title"] = "Elastic Heart";
+                row["Artist"] = "Sia";
+                row["Album"] = "1000 Forms of Fear";
+                row["Length"] = "5:04";
+                row["Plays"] = "17";
+                TrackData.Rows.Add(row);
+            }
         }
     }
 }
